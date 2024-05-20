7ccd05f73356be48d39646a5856ce1275b79d99b
magdalenabrinas <152439140+magdalenabrinas@users.noreply.github.com>
2024-05-20 17:05:15 -0600

    Added week-2 files

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import StudentInfo from './student-info'; // Adjust the path if necessary
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Magdalena Brinas</h1>
+      <p>
+        <Link href="https://github.com/magdalenabrinas">My GitHub Repository</Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
