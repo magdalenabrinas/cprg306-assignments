7beb55a99697f86acb22569ed2800656d55a1285
magdalenabrinas <152439140+magdalenabrinas@users.noreply.github.com>
2024-05-20 22:38:21 -0600

    modify page.js

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
-        <Link href="/week-2">Week-2</Link>
-      </p>
-    </div>
+    <main className="flex min-h-screen flex-col p-24">
+      <div>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <p>
+          <Link href="/week-2">Week-2 Assignment</Link>
+        </p>
+      </div>
+    </main>
   );
 }
