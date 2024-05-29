1bf4d0252eb58636acaddb1ef98bcd07d0bfa2ab
magdalenabrinas <152439140+magdalenabrinas@users.noreply.github.com>
2024-05-28 22:34:51 -0600

    week 3 update

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Item = ({ name, quantity, category }) => {
   return (
-    <li className="flex items-center justify-between p-2 border-b border-black bg-white mb-2">
-      <div className="w-3/4">
+    <li className="w-1/4 flex items-center justify-between p-2 border-b bg-white mb-2 border-black rounded">
+      <div>
         <h3 className="text-base font-semibold text-black">{name}</h3>
         <p className="text-xs text-yellow-600">Category: <span className="text-black">{category}</span></p>
       </div>
-      <span className="text-base font-bold text-black">{quantity}</span>
+      <div className="text-base font-bold text-black">{quantity}</div>
     </li>
   );
 };
 
 export default Item;
+ 
